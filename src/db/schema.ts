b58865ee153effcdcd67787603b92a0a3069669b
meph <mephju@gmail.com>
2024-10-30 18:30:25 +0100

    main

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,16 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { int, sqliteTable as table, text } from "drizzle-orm/sqlite-core";
+import { createId } from "@paralleldrive/cuid2";
 
-export const usersTable = sqliteTable("users_table", {
+export const usersTable = table("users", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
   age: int().notNull(),
   email: text().notNull().unique(),
+});
+
+export const productsTable = table("products", {
+  id: text()
+    .primaryKey()
+    .$default(() => createId()),
+  name: text().notNull(),
 });
